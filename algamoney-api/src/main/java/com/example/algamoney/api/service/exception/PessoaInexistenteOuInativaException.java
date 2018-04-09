a89a6f3f548433ce8f62f8ed4598ac968da404be
fakemarciano <fakemarciano@gmail.com>
2018-04-08 22:18:30 -0300

    API de controle de gastos pessoais feita em Spring

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/service/exception/PessoaInexistenteOuInativaException.java
@@ -0,0 +1,10 @@
+package com.example.algamoney.api.service.exception;
+
+public class PessoaInexistenteOuInativaException extends RuntimeException {
+
+	/**
+	 * UID
+	 */
+	private static final long serialVersionUID = 5062781048514301324L;
+
+}
